56084a1fedc655ce69f204d52edf6992e897fb80
PermissionDog <455662503@qq.com>
2023-11-04 11:41:02 +0800

    refactor: 重构网络请求目录，添加登录 API 示例

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,4 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { login } from '@/api';
+
+// login example
+
+const loginExample = async () => {
+  try {
+    const user = await login({
+      username: 'admin',
+      password: '123456',
+    })
+    console.log(user, '登录成功')
+  } catch (e) {
+    console.log('登录失败')
+  }
+}
+
+</script>
 
 <template>
   <h1>LOGIN...</h1>
