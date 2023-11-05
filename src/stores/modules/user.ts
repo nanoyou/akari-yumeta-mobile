9a7a3d988a5f650086f036ee14c5e258ec650192
w2002 <w2002@TABLET-ESK3P9AM>
2023-11-05 10:40:33 +0800

    加入注释

--- src/stores/modules/user.ts
+++ src/stores/modules/user.ts
@@ -1,16 +1,23 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+// 使用pinia库定义一个名为useUserStore的状态管理器
 export const useUserStore = defineStore(
-  'user',
+  'user', // 状态管理器的名称
   () => {
+    // 定义一个名为token的响应式变量，并初始化为空字符串
     const token = ref('')
+    // 定义一个名为ID的响应式变量，并初始化为空字符串
     const ID = ref('')
+    // 定义一个名为setToken的函数，接收一个字符串参数tk，并将token变量的值设置为传入的参数tk
     const setToken = (tk: string) => (token.value = tk)
+    // 定义一个名为setID的函数，接收一个字符串参数id，并将ID变量的值设置为传入的参数id
     const setID = (id: string) => (ID.value = id)
+    // 返回一个包含token、setToken、ID和setID的对象
     return { token, setToken, ID, setID }
   },
+  // 设置状态管理器的额外选项
   {
-    persist: true
+    persist: true // 指定状态管理器应该保持持久化
   }
 )
