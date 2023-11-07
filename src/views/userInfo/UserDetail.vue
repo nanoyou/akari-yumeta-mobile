ec5ec4dc8ec4344f09105a492caafbfcbb231ac4
PermissionDog <455662503@qq.com>
2023-11-07 20:58:20 +0800

    feat: 添加登录按钮

--- src/views/userInfo/UserDetail.vue
+++ src/views/userInfo/UserDetail.vue
@@ -4,6 +4,8 @@ import { type UserDTO } from '@/api/entity'
 import { ref } from 'vue'
 import { watch } from 'vue'
 import CommonCard from '@/components/user/CommonCard.vue'
+import { useUserStore } from '@/stores'
+import router from '@/router'
 const props = defineProps<{
   userID: string
   perspective: 'me' | 'others'
@@ -11,16 +13,29 @@ const props = defineProps<{
 
 const user = ref<UserDTO>()
 ;(async () => (user.value = await getUserInfo(props.userID)))()
+
+const userStore = useUserStore()
+
+const logout = () => {
+  userStore.logout()
+  router.push('/login')
+}
 </script>
 
 <template>
   <div class="info-page">
     <CommonCard :user="user!" />
+    <van-cell-group>
+      <van-cell title="退出登录" @click="logout" />
+    </van-cell-group>
   </div>
 </template>
 
 <style scoped>
 .info-page {
   display: flex;
+  width: 100%;
+
+  flex-direction: column;
 }
 </style>
