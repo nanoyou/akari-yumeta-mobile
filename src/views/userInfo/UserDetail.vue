6a532ae7ee90237b1295a7d76ca27d70be338ad1
PermissionDog <455662503@qq.com>
2023-11-08 13:39:45 +0800

    feat: 美化退出按钮

--- src/views/userInfo/UserDetail.vue
+++ src/views/userInfo/UserDetail.vue
@@ -26,7 +26,26 @@ const logout = () => {
   <div class="info-page">
     <CommonCard :user="user!" />
     <van-cell-group>
-      <van-cell title="退出登录" @click="logout" />
+      <van-cell
+        v-if="perspective == 'me'"
+        title="退出登录"
+        @click="logout"
+        clickable
+        is-link
+      >
+        <template #icon>
+          <div
+            style="
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              margin-right: 5px;
+            "
+          >
+            <van-icon name="/icon/quit.svg" />
+          </div>
+        </template>
+      </van-cell>
     </van-cell-group>
   </div>
 </template>
