fc8dac6d1bddb77d14b0bb999b33fd7dc0903c5e
PermissionDog <455662503@qq.com>
2023-11-09 19:40:19 +0800

    feat: 完成聊天导航栏

--- src/views/chat/ChatDialog.vue
+++ src/views/chat/ChatDialog.vue
@@ -1,101 +1,88 @@
 <script setup lang="ts">
-
-import ChatDialogBar from "@/components/tabbar/ChatDialogBar.vue";
-import {ref} from "vue";
+import ChatDialogBar from '@/views/chat/ChatDialogBar.vue'
+import { ref } from 'vue'
 
 const message_value = ref('')
 </script>
 
 <template>
-  <ChatDialogBar></ChatDialogBar>
-  <div class="dialog_body">
-    <div class="message_container_left">
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-      <div class="message_container_white">
-        <div class="message_word_black">
-          我测你妈
+  <div class="chat-dialog">
+    <ChatDialogBar></ChatDialogBar>
+    <div class="dialog_body">
+      <div class="message_container_left">
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
+        <div class="message_container_white">
+          <div class="message_word_black">我测你妈</div>
         </div>
       </div>
-    </div>
 
-    <div class="message_container_right">
-      <div class="message_container_blue">
-        <div class="message_word_white">
-          我测你妈
+      <div class="message_container_right">
+        <div class="message_container_blue">
+          <div class="message_word_white">我测你妈</div>
         </div>
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
       </div>
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-    </div>
-    <div class="message_container_right">
-      <div class="message_container_blue">
-        <div class="message_word_white">
-          我测你妈
+      <div class="message_container_right">
+        <div class="message_container_blue">
+          <div class="message_word_white">我测你妈</div>
         </div>
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
       </div>
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-    </div>
-    <div class="message_container_right">
-      <div class="message_container_blue">
-        <div class="message_word_white">
-          我测你妈
+      <div class="message_container_right">
+        <div class="message_container_blue">
+          <div class="message_word_white">我测你妈</div>
         </div>
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
       </div>
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-    </div>
 
-    <div class="message_container_left">
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-      <div class="message_container_white">
-        <div class="message_word_black">
-          我测你妈
+      <div class="message_container_left">
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
+        <div class="message_container_white">
+          <div class="message_word_black">我测你妈</div>
         </div>
       </div>
-    </div>
-    <div class="message_container_left">
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-      <div class="message_container_white">
-        <div class="message_word_black">
-          我测你妈
+      <div class="message_container_left">
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
+        <div class="message_container_white">
+          <div class="message_word_black">我测你妈</div>
         </div>
       </div>
-    </div>
-    <div style="justify-content: center; display: flex">
-      <div class="time_message">10-29 14:59</div>
-    </div>
-    <div class="message_container_right">
-      <div class="message_container_blue">
-        <div class="message_word_white">
-          我测你妈
-        </div>
+      <div style="justify-content: center; display: flex">
+        <div class="time_message">10-29 14:59</div>
       </div>
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-    </div>
-    <div class="message_container_right">
-      <div class="message_container_blue">
-        <div class="message_word_white">
-          我测你妈
+      <div class="message_container_right">
+        <div class="message_container_blue">
+          <div class="message_word_white">我测你妈</div>
         </div>
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
       </div>
-      <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
-    </div>
+      <div class="message_container_right">
+        <div class="message_container_blue">
+          <div class="message_word_white">我测你妈</div>
+        </div>
+        <img class="search_record_photo" src="/imgs/teacher1.jpg" alt="" />
+      </div>
 
-    <div class="search_container">
-      <van-cell-group inset>
-        <van-field
+      <div class="search_container">
+        <van-cell-group inset>
+          <van-field
             style="height: 35px; width: 260px"
             v-model="message_value"
-        />
-      </van-cell-group>
-      <van-button class="send_button">发送</van-button>
+          />
+        </van-cell-group>
+        <van-button class="send_button">发送</van-button>
+      </div>
     </div>
-
   </div>
 </template>
 
 <style scoped>
+.chat-dialog {
+  width: 100%;
+}
 .send_button {
-  background-color: #7ACBFA;
-  color: #C0E4FC;
+  background-color: #7acbfa;
+  color: #c0e4fc;
   border-radius: 10px;
   height: 35px;
 }
@@ -106,11 +93,11 @@ const message_value = ref('')
 }
 
 .time_message {
-  color: #93929A;
-  margin: 8px 0px 8px 0px
+  color: #93929a;
+  margin: 8px 0px 8px 0px;
 }
-.message_container_blue{
-  background-color: #0199FE;
+.message_container_blue {
+  background-color: #0199fe;
   margin: 10px;
   height: 46px;
   line-height: 46px;
@@ -141,7 +128,7 @@ const message_value = ref('')
 }
 .dialog_body {
   height: 85vh;
-  background-color: #F1F1F1;
+  background-color: #f1f1f1;
   padding-top: 10px;
 }
 .search_record_photo {
