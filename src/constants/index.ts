3158ee3fcf9930504c27455a927dbd95ad5981a0
PermissionDog <455662503@qq.com>
2023-11-11 11:59:11 +0800

    refactor: 修改地址常量

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,2 @@
 export const VIDEO_CALL_HOST = 'http://localhost:8888'
-export const HTTP_HOST = 'http://104.156.230.230:8080'
-// export const HTTP_HOST = 'http://172.22.26.81:8080'
+export const HTTP_HOST = 'http://localhost:8080'
